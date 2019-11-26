fc696d8db9a7e833fa704c38e049f228ded8455d
Han Shao <hshao@oberlin.edu>
2019-11-26 17:22:03 -0500

    test

--- mm.py
+++ mm.py
@@ -102,7 +102,7 @@ def main():
         generated_event = model.predict(init_temp, batch_size=1)
         print(init.shape)
         print(generated_event.shape)
-        init = np.append(init[1:], generated_event, axis=1)
+        init = np.append(init[1:], generated_event, axis=0)
         generated_sequence.append(generated_event)
     print(len(generated_sequence))
 
