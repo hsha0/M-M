b4dae0af1da033d2a66af33a6e02d9a92a9be827
Han Shao <hshao@oberlin.edu>
2019-11-29 13:50:30 -0500

    test

--- mm.py
+++ mm.py
@@ -182,7 +182,7 @@ def main():
                 if epoch > cur_epoch: cur_epoch = epoch
 
             model_name = 'model_' + str(cur_epoch) + '.ckpt'
-            model = tf.keras.Model.load_model(model_name)
+            model = tf.keras.models.load_model(model_name)
             print("Load model:", model_name)
             if FLAGS.num_epochs > cur_epoch:
                 FLAGS.num_epochs = FLAGS.num_epochs - cur_epoch
