e0572a8ec3d5a536b4c071ec9b9399d1056a8dc7
Han Shao <hshao@oberlin.edu>
2019-11-28 22:30:19 -0500

    test

--- enc_dec.py
+++ enc_dec.py
@@ -157,12 +157,14 @@ def main():
     generated_seq = []
     for i in range(int(FLAGS.num_generate_events/FLAGS.interval)):
         result = model.predict(x=[init, init], batch_size=1)
+        seq = np.reshape(np.argmax(result, axis=2)[0], (FLAGS.interval, 3))
+
         print(result)
         init = np.argmax(result, axis=2).flatten()
         print(init.shape)
 
 
-        seq = np.reshape(np.argmax(result, axis=2)[0],(FLAGS.interval, 3))
+
         generated_seq.extend(seq)
     print(generated_seq)
 
